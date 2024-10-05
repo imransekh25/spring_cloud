afcadf620721fab2ef0b00c5a7b3ea837fff027b
imran.sekh <imransekh25@gmail.com>
2024-10-05 19:10:07 +0530

    new project

---
+++ src/main/java/com/botpay/gateway/BotPayGateApplication.java
@@ -0,0 +1,13 @@
+package com.botpay.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotPayGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotPayGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botpay/gateway/BotPayGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botpay.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotPayGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
